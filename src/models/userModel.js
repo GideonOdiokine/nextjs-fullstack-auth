39ff372568ab24df0afb99827e3bd9cfb2dc7b87
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-08-20 03:51:40 +0100

    added page layout

---
+++ src/models/userModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  isVerfied: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+//   crime: [
+//     {
+//       name: String,
+//       type: {
+//         type: String
+//       },
+//       registrationEnabled: Boolean,
+//       checkinEnabled: Boolean
+//     }
+//   ],
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
