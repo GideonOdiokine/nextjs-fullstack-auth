8c38b8b3e8f9f6d34f732578d077c4f6b8e518a8
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-08-22 04:42:14 +0100

    Develop Login APIs route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,60 @@
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { connect } from '@/dbconfig/dbConfig';
+import jwt from 'jsonwebtoken'
+// import { sendEmail } from "@/helpers/mailer";
+
+
+connect()
+
+export async function POST(request: NextRequest) {
+    try {
+
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
+        console.log(reqBody)
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return NextResponse.json({ error: "User does not exist" }, { status: 400 })
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password)
+
+        if (!validPassword) {
+            return NextResponse.json({
+                error: "Invalid password"
+            }, { status: 400 })
+        }
+
+        // create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+        // Create token
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" })
+
+        const response= NextResponse.json({
+            message:"Login Successful",
+            success:true
+        })
+
+        response.cookies.set("token", token, {
+            httpOnly:true,
+        })
+
+        return response
+
+    } catch (error: any) {
+        return NextResponse.json({
+            error: error.message
+        }, {
+            status: 500
+        })
+    }
+}
+
