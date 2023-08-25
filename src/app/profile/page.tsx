abf89321e1ce7fa02bf134b6dca0b65da100e22f
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-08-25 05:25:20 +0100

    Implemented Logout API on profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,18 +6,18 @@ import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
-//   const router = useRouter();
+  const router = useRouter();
 //   const [data, setData] = useState("nothing");
-//   const logout = async () => {
-//     try {
-//       await axios.get("/api/users/logout");
-//       toast.success("Logout successful");
-//       router.push("/login");
-//     } catch (error: any) {
-//       console.log(error.message);
-//       toast.error(error.message);
-//     }
-//   };
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
 
 //   const getUserDetails = async () => {
 //     const res = await axios.get("/api/users/me");
@@ -40,7 +40,7 @@ export default function ProfilePage() {
       </h2>
       <hr />
       <button
-        // onClick={logout}
+        onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Logout
